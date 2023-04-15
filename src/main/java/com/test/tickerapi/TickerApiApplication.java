d06f24ce4ad3ad343ff3792f698c84c5bf4acb7b
povia <47127607+povia@users.noreply.github.com>
2023-04-15 18:46:50 +0900

    init: project setup

---
+++ src/main/java/com/test/tickerapi/TickerApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.tickerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/tickerapi/TickerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tickerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
